a4b438c32a9a9b0addb3baba242530e4bd27e8ec
Ming Mak <63583791+ming019@users.noreply.github.com>
2025-07-04 20:30:39 +0800

    Update install-docker-compose.sh

--- install-docker-compose.sh
+++ install-docker-compose.sh
@@ -4,7 +4,7 @@ apt upgrade -y
 apt -y install apt-transport-https ca-certificates curl software-properties-common
 apt -y remove docker docker-engine docker.io containerd runc
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-archive-keyring.gpg
-echo "deb https://download.docker.com/linux/debian bookworm stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb https://download.docker.com/linux/debian bookworm stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
 apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 apt clean
