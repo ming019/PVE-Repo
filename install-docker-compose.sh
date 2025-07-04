217ac3575f5699a50a1061a3dfcae1bd85c10c4a
Ming Mak <63583791+ming019@users.noreply.github.com>
2025-07-04 19:53:01 +0800

    Add files via upload

---
+++ install-docker-compose.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apt update
+apt upgrade -y
+apt -y install apt-transport-https ca-certificates curl software-properties-common
+apt -y remove docker docker-engine docker.io containerd runc
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-archive-keyring.gpg
+echo "deb https://download.docker.com/linux/debian bookworm stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+docker version
